9c660ae814265e7a8e24d4c341e0cd2c8702aa4d
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-01-18 00:23:33 -0500

    Added API access

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,29 @@
 import { signIn, getUser } from '../js/auth';
+import { getUserFragments } from "../js/api";
 
 async function init() {
-  // Get our UI elements
-  const userSection = document.querySelector('#user');
-  const loginBtn = document.querySelector('#login');
+    // Get our UI elements
+    const userSection = document.querySelector('#user');
+    const loginBtn = document.querySelector('#login');
 
-  // Wire up event handlers to deal with login and logout.
-  loginBtn.onclick = () => {
+    // Wire up event handlers to deal with login and logout.
+    loginBtn.onclick = () => {
     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
     signIn();
-  };
+    };
 
-  // See if we're signed in (i.e., we'll have a `user` object)
-  const user = await getUser();
-  if (!user) {
+    // See if we're signed in (i.e., we'll have a `user` object)
+    const user = await getUser();
+    if (!user) {
     return;
-  }
+    }
 
-  loginBtn.disabled = true;
-  userSection.hidden = false;
-  userSection.querySelector('.username').innerText = user.username;
+    const userFragments = await getUserFragments(user);
+    console.log({userFragments}, `User Fragments`);
+
+    loginBtn.disabled = true;
+    userSection.hidden = false;
+    userSection.querySelector('.username').innerText = user.username;
 }
 
 // Wait for the DOM to be ready, then start the app
